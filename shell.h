23e33ce583168dd5bd102d71fe2be13719ce166d
Sabeem <117848369+Sabeem101@users.noreply.github.com>
2023-05-24 23:07:28 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,18 +1,3 @@
-
-#ifndef SHELL_H
-#define SHELL_H
-
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <stdlib.h>
-
-int calculate_area_rect(int length, int breath)
-	
-=======
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
